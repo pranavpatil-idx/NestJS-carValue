369f517ae0f9bb555626cf01d0ffa63440d1ef7b
pranavpatil-idx <pranav.patil@indexnine.com>
2025-09-16 14:42:51 +0530

    Authentication Done

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,48 +1,86 @@
-import { Controller, Post, Body, Patch, Param, Get, Query, Delete, NotFoundException } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Get,
+  Query,
+  Delete,
+  NotFoundException,
+  Session,
+  UseGuards
+} from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dtos/update-user.dto';
-import { Serialize} from '../interceptors/serialize.interceptor';
+import { Serialize } from '../interceptors/serialize.interceptor';
 import { UserDto } from './dtos/user.dto';
+import { AuthService } from './auth.service';
+import { CurrentUser } from './decorators/current-user-decorator';
+import { User } from './user.entity';
+import { AuthGuard } from '../guards/auth.guard';
 
 @Controller('auth')
 @Serialize(UserDto)
 export class UsersController {
+  constructor(
+    private usersService: UsersService,
+    private authService: AuthService,
+  ) {}
 
-    constructor(private usersService: UsersService){}
+  @Get()
+  listUsers(@Query('email') email: string) {
+    return this.usersService.find(email);
+  }
 
-    // @UseInterceptors(SerializeInterceptor)
-    // @Serialize(UserDto)
-    @Get()
-    listUsers(@Query('email') email: string) {
-        return this.usersService.find(email);
+  // @Get('/whoami')
+  // whoAmI(@Session() session: any) {
+  //   return this.usersService.findOne(session.userId);
+  // }
+
+  @UseGuards(AuthGuard)
+  @Get('/whoami')
+  whoamI(@CurrentUser() user: User) {
+    return user;
+  }
+
+  @Post('/signup')
+  async createUser(@Body() body: CreateUserDto, @Session() session: any) {
+    const user = await this.authService.signup(body.email, body.password);
+    session.userId = user.id;
+    return user;
+  }
+
+  @Post('/signin')
+  async signin(@Body() body: CreateUserDto, @Session() session: any) {
+    const user = await this.authService.signin(body.email, body.password);
+    session.userId = user.id;
+    return user;
+  }
+
+  @Post('/signout')
+  signout(@Session() session: any) {
+    session.userId = null;
+    return { message: 'Successfully signed out' };
+  }
+
+  @Get('/:id')
+  async getUser(@Param('id') id: string) {
+    const user = await this.usersService.findOne(parseInt(id));
+    if (!user) {
+      throw new NotFoundException('User not found');
     }
+    return user;
+  }
 
-    // @UseInterceptors(new SerializeInterceptor(UserDto))
-    @Get('/:id')
-    // @Serialize(UserDto)
-    async getUser(@Param('id') id: string) {
-        console.log('Handler is running');
-        const user = await this.usersService.findOne(parseInt(id));
-        if(!user){
-            throw new NotFoundException('User not found');
-        }
-        return user;
-    }
+  @Delete('/:id')
+  removeUser(@Param('id') id: string) {
+    return this.usersService.remove(parseInt(id));
+  }
 
-    @Post('/signup')
-    createUser(@Body() body : CreateUserDto){
-        this.usersService.create(body.email, body.password)
-    }
-
-    @Delete('/:id')
-    removeUser(@Param('id') id: string) {
-        return this.usersService.remove(parseInt(id));
-    }
-
-    @Patch('/:id')
-    updateUser(@Param('id') id: string, @Body() body: Partial<UpdateUserDto>) {
-        return this.usersService.update(parseInt(id), body);
-    }
-
+  @Patch('/:id')
+  updateUser(@Param('id') id: string, @Body() body: Partial<UpdateUserDto>) {
+    return this.usersService.update(parseInt(id), body);
+  }
 }
